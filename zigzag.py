daa40eb68fb19a720ca331e7809e9a197bb72e5f
sunilkumar k <91720945+Sunilanivala@users.noreply.github.com>
2021-10-01 13:01:25 +0530

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,19 @@
+# import time,sys
+# indent = 0
+# indentincrement = True
+# try:
+#     while True:
+#         print(' ' * indent,end="")
+#         print('******')
+#         time.sleep(0.2)
+#         if  indentincrement:
+#             indent += 1
+#             if indent == 20:
+#                 indentincrement = False
+#         else:
+#             indent -= 1
+#             if indent == 0:
+#                 indentincrement = True
+# except KeyboardInterrupt:
+#     sys.exit()
+
